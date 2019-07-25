0f314b28f67dcb39b867b1cb5661a159d6eab31f
David <digital0david@gmail.com>
2019-07-24 22:10:35 -0400

    get all cars

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -6,6 +6,16 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/cars", (res, res) => {
+  Cars.getAll()
+    .then(cars => {
+      res.status(200).json(cars);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+})
+
 server.post("/cars", async (req, res) => {
   try {
     const car = await Cars.insert(req.body);
